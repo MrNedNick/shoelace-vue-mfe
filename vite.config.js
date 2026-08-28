0b9aafe66777530f3ee5021dc0491cd75f897905
MrNedNick <mrnediala@gmail.com>
2026-08-28 10:10:36 +0200

    chore: scaffold Vite + Vue 3 project

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue({ customElement: true })],
+  build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'ShoelaceVueMfe',
+      fileName: () => 'shoelace-vue-mfe.js',
+      formats: ['es']
+    }
+  }
+})
